79d4bcc42c4f9ef3a13e857053601d701f668347
Amitav Roy <reachme@amitavroy.com>
2019-10-17 07:18:57 +0530

    #2 worked on the navigation and handled the login functionality through axios right now

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        <p>This is Dashboard, welcome {{fname}}, {{lname}}</p>
-    </div>
+    <layout>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>This is Dashboard, welcome {{fname}}, {{lname}}</p>
+                </div>
+            </div>
+        </div>
+    </layout>
 </template>
 
 <script>
+import Layout from './../../Shared/Layout'
 export default {
+    components: {
+        Layout
+    },
     props: ['fname', 'lname']
 }
 </script>
---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <inertia-link class="navbar-brand" href="/">FitApp</inertia-link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Amitav Roy <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#" @click="handleLogout">Logout</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    methods: {
+        async handleLogout() {
+            await axios.post('/logout', {});
+            window.location.href = "/";
+        }
+    }
+}
+</script>
