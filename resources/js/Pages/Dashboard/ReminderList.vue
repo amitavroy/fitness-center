a4166d8883a6d88c0a75127391afd1191b684826
Amitav Roy <reachme@amitavroy.com>
2020-01-11 08:30:03 +0530

    handling a message when there are no reminders on dashboard

--- resources/js/Pages/Dashboard/ReminderList.vue
+++ resources/js/Pages/Dashboard/ReminderList.vue
@@ -1,24 +1,25 @@
 <template>
-    <div class="card">
-        <div class="card-header">Reminders list</div>
-        <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item" v-for="reminder in reminders" :key="reminder.id">
-                    <inertia-link :href="$route('reminder.view', {lead: reminder.lead, reminder: reminder})">
-                        <strong>{{reminder.lead.name}}</strong>
-                        <br>
-                        {{reminder.reminder}}
-                    </inertia-link>
-                </li>
-            </ul>
-        </div>
+  <div class="card">
+    <div class="card-header">Reminders list</div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush" v-if="reminders.length > 0">
+        <li class="list-group-item" v-for="reminder in reminders" :key="reminder.id">
+          <inertia-link :href="$route('reminder.view', {lead: reminder.lead, reminder: reminder})">
+            <strong>{{reminder.lead.name}}</strong>
+            <br />
+            {{reminder.reminder}}
+          </inertia-link>
+        </li>
+      </ul>
+      <div v-else>You have no reminders for today.</div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            reminders: Array
-        }
-    }
+export default {
+  props: {
+    reminders: Array
+  }
+};
 </script>
